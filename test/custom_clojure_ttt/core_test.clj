f2c1ee9c9cf0f0dff669278170dbb7e651385dba
Jon Chin <jchin@8thlight.com>
2017-12-04 10:12:33 -0800

    Initialize Git repo

---
+++ test/custom_clojure_ttt/core_test.clj
@@ -0,0 +1,7 @@
+(ns custom-clojure-ttt.core-test
+  (:require [clojure.test :refer :all]
+            [custom-clojure-ttt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
