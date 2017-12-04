9342e0e672bfa592c1504a50086a2f48f0b2c239
Jon Chin <jchin@8thlight.com>
2017-12-04 10:49:19 -0800

    Refresh repl using '(user/rerun)'

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot custom-clojure-ttt.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.namespace "0.2.11"]]
+  :profiles {:dev {:source-paths ["startup"]}
+             :uberjar {:aot :all}}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :main ^:skip-aot custom-clojure-ttt.core)
