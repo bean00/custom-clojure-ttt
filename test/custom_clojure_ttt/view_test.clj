302199c9d1d8e9f95f38167c4976525fe2227de8
Jon Chin <jchin@8thlight.com>
2017-12-06 10:43:06 -0800

    Copy view files over

---
+++ test/custom_clojure_ttt/view_test.clj
@@ -0,0 +1,26 @@
+(ns custom-clojure-ttt.view_test
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure-tic-tac-toe.game_handler :as game_handler]
+            [clojure-tic-tac-toe.utilities :refer [join-lines]]
+            [custom-clojure-ttt.view :refer :all]))
+
+(deftest create-view-test
+  (testing "when an empty board is passed in"
+    (let [view (join-lines [" 1 | 2 | 3 "
+                            "---+---+---"
+                            " 4 | 5 | 6 "
+                            "---+---+---"
+                            " 7 | 8 | 9 "])]
+      (is (= view
+             (create-view game_handler/empty-board))
+          "it returns the formatted string for the example board")))
+  (testing "when a game state with a board is passed in"
+    (let [view (join-lines [" X | 2 | X "
+                            "---+---+---"
+                            " 4 | O | 6 "
+                            "---+---+---"
+                            " 7 | 8 | 9 " ])]
+      (is (= view
+             (create-view {:board {:X #{:1 :3}, :O #{:5}}}))
+          "it returns the formatted string for the board"))))
+
