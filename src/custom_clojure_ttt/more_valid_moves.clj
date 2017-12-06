267a7a17c2d670e2707a26de40302cfdb29a4640
Jon Chin <jchin@8thlight.com>
2017-12-06 12:06:26 -0800

    Add more_valid_moves.clj, with more_valid_moves data structure

---
+++ src/custom_clojure_ttt/more_valid_moves.clj
@@ -0,0 +1,12 @@
+(ns custom-clojure-ttt.more_valid_moves)
+
+(def more_valid_moves
+  { :3x3 #{:1 :2 :3
+           :4 :5 :6
+           :7 :8 :9}
+
+    :4x4 #{ :1  :2  :3  :4
+            :5  :6  :7  :8
+            :9 :10 :11 :12
+           :13 :14 :15 :16} })
+
