01a765a6268decba3f067f70ddcb8c0e2ed040e2
Jon Chin <jchin@8thlight.com>
2017-12-11 17:05:21 -0800

    Use default_valid_moves from core project

--- src/custom_clojure_ttt/valid_move_handler.clj
+++ src/custom_clojure_ttt/valid_move_handler.clj
@@ -1,9 +1,8 @@
-(ns custom-clojure-ttt.valid_move_handler)
+(ns custom-clojure-ttt.valid_move_handler
+  (:require [clojure-tic-tac-toe.default_valid_moves :as default_valid_moves]))
 
 (def valid-moves
-  { :3 #{:1 :2 :3
-         :4 :5 :6
-         :7 :8 :9} })
+  { :3 default_valid_moves/valid-moves })
 
 (defn get-valid-moves
   [side-length]
--- test/custom_clojure_ttt/valid_move_handler_test.clj
+++ test/custom_clojure_ttt/valid_move_handler_test.clj
@@ -1,14 +1,18 @@
 (ns custom-clojure-ttt.valid_move_handler_test
   (:require [clojure.test :refer [deftest testing is]]
+            [clojure-tic-tac-toe.default_valid_moves :as default_valid_moves]
             [custom-clojure-ttt.valid_move_handler :refer :all]))
 
+(def valid-moves-on-3x3
+  default_valid_moves/valid-moves)
+
 (deftest get-valid-moves-test
-  (testing "when a side length is passed in"
-    (is (= #{:1 :2 :3 :4 :5 :6 :7 :8 :9}
+  (testing "when choosing a side length of 3"
+    (is (= valid-moves-on-3x3
            (get-valid-moves :3))
-        "it returns the valid moves"))
+        "it returns the valid moves for a 3x3 board"))
   (testing "when choosing the default size"
-    (is (= #{:1 :2 :3 :4 :5 :6 :7 :8 :9}
+    (is (= valid-moves-on-3x3
            (get-valid-moves :default-size))
-        "it returns the moves for a 3x3 board")))
+        "it returns the valid moves for a 3x3 board")))
 
