2b48c24c2041d77385433d1c2fa5e9e42f7f1e32
Jon Chin <jchin@8thlight.com>
2017-12-15 15:10:57 -0800

    Add tests for valid-moves to handler

--- test/custom_clojure_ttt/valid_move_handler_test.clj
+++ test/custom_clojure_ttt/valid_move_handler_test.clj
@@ -3,6 +3,23 @@
             [clojure.test :refer [deftest testing is]]
             [custom-clojure-ttt.valid_move_handler :refer :all]))
 
+(deftest valid-moves-3x3-test
+  (testing "when getting the valid moves for a 3x3 board"
+    (is (= #{:1 :2 :3
+             :4 :5 :6
+             :7 :8 :9}
+           valid-moves-3x3)
+        "it returns all the valid moves")))
+
+(deftest valid-moves-4x4-test
+  (testing "when getting the valid moves for a 4x4 board"
+    (is (= #{  :1  :2  :3  :4
+               :5  :6  :7  :8
+               :9 :10 :11 :12
+              :13 :14 :15 :16}
+           valid-moves-4x4)
+        "it returns all the valid moves")))
+
 (deftest get-valid-moves-test
   (testing "when choosing a side length of 3"
     (is (= true
